fd4bbe14c14ed273cc03ae1ac23e5f5ecfa8c571
유영진 <ryuyj76@naver.com>
2019-11-12 17:16:34 +0900

    다형성을 이용한 배열

--- java_project/FIrstJAVA/src/shop/Product.java
+++ java_project/FIrstJAVA/src/shop/Product.java
@@ -20,13 +20,35 @@ public class Product {
 		Computer com = new Computer(300);
 		Product p2 = com;
 		
+		Audio aud = new Audio(50);
+		
+		b.buy(aud);
 		b.buy(tv);
 		b.buy(com);
+		
+		b.summary();
+		
+		System.out.println("================================");
 		
 		System.out.println("현재 남은 돈 : " + b.money);
 		System.out.println("현재 보너스 포인트 : " + b.bonusPoint);
 		
 	}
+	
+	
+}
+
+class Audio extends Product {
+
+	public Audio(int price) {
+		super(price);
+	}
+
+	@Override
+	public String toString() {
+		return "Audio";
+	}
+	
 	
 	
 }
@@ -64,6 +86,11 @@ class Buyer {
 	int money = 1000;
 	int bonusPoint = 0;
 	
+	// 장바구니
+	Product[] cart = new Product[10];
+	// 구매 상품의 개수, 배열에 입력할때 index 값으로 사용, 반복문 사용시 반복의 조건
+	int cnt = 0;	
+	
 	void buy(Product p) {
 		
 		if(money < p.price) {
@@ -74,8 +101,31 @@ class Buyer {
 		money = money - p.price;
 		bonusPoint = bonusPoint + p.bonusPoint;
 		
+		// 바구니에 추가
+		cart[cnt++] = p;
+		// 구애 상품의 개수 1 증가
+		// cnt++;
+		
+		
 		System.out.println(p + "을/를 구입하셨습니다.");
 		
+	}
+	
+	void summary() {
+		// 구매 상품의 총 금액
+		int sum = 0;
+		// 구매 제품 리스트
+		String productList = "";
+		
+		for(int i=0; i<cnt; i++) {
+			// 제품 가격을 더한다.
+			sum += cart[i].price;
+			// 제품리스트
+			productList += cart[i] + ", ";
+		}
+		
+		System.out.println("구입한 제품의 총 금액 : " + sum + "원 입니다.");
+		System.out.println("구입한 제품의 목록은 " + productList + "입니다.");
 	}
 	
 }
