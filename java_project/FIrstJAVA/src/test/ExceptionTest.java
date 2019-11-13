09cdaa7f1582cda07f5f72fdc0439995e2284bd3
유영진 <ryuyj76@naver.com>
2019-11-13 16:51:59 +0900

    예외처리

---
+++ java_project/FIrstJAVA/src/test/ExceptionTest.java
@@ -0,0 +1,36 @@
+package test;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		
+		int number = 100;
+		int result = 0;
+		
+		for(int i=0; i<10; i++) {
+			try {
+				result= number/(int)(Math.random()*10);
+				System.out.println(result);
+			} catch (ArithmeticException ae) {
+				//System.out.println("0으로 나누기는 할수 없습니다.");
+				i--;
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
